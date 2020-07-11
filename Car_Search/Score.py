0f2bbc90fe874252bebbacc743854fc935642f26
ViniciusOliveiraAndrade <viniciusibsm@gmail.com>
2020-07-10 23:20:03 -0300

    Updated the Score to O.O.

--- Car_Search/Score.py
+++ Car_Search/Score.py
@@ -2,3 +2,10 @@ class Score():
 
     def __init__(self, score):
         self.score = score
+    
+    def display(self):
+        fill(0)
+        text("Score: {0}".format(self.score),2,11)
+        
+    def up(self):
+        self.score+=1
