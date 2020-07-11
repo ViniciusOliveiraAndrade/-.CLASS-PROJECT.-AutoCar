3c3865cf8293b2b560ea38b8ffc0fbdf75bd5dd7
ViniciusOliveiraAndrade <viniciusibsm@gmail.com>
2020-07-10 23:18:17 -0300

    Clean e updated Food class

--- Car_Search/Food.py
+++ Car_Search/Food.py
@@ -2,40 +2,24 @@
 
 class Food():
 
-    def __init__(self, x, y, vel, r):
-        self.acceleration = PVector(0, 0)
-        self.velocity = vel
-        self.position = PVector(x, y)
+    def __init__(self, i, j, r):
+        self.i = int(i)
+        self.j = int(j)
         self.r = r
-        self.maxspeed = 1.0
-        self.maxforce = 0.01
 
     # Method to update location
-    def update(self):
-        # Update velocity
-        self.velocity.add(self.acceleration)
-        # Limit speed
-        self.velocity.limit(self.maxspeed)
-        self.position.add(self.velocity)
-        # Reset accelerationelertion to 0 each cycle
-        self.acceleration.mult(0)
-
-    def applyForce(self, force):
-        # We could add mass here if we want A = F / M
-        self.acceleration.add(force)
-
+    def updatePosition(self, i, j):
+        self.i = int(i)
+        self.j = int(j)
+        print("i: {} j: {}".format(int(i),int(j)))
+    
+    def getX(self):
+         return self.j * self.r
+    def getY(self):
+        return self.i * self.r
+        
     def display(self):
-        # Draw a triangle rotated in the direction of velocity
-        theta = self.velocity.heading()# + PI / 2
         fill(127)
         noStroke()
-        strokeWeight(1)
-        with pushMatrix():
-            translate(self.position.x, self.position.y)
-            rotate(theta)
-            rect((self.r/2)*-1, (self.r/2)*-1, self.r, self.r)
-            # beginShape()
-            # vertex(0, -self.r * 2)
-            # vertex(-self.r, self.r * 2)
-            # vertex(self.r, self.r * 2)
-            # endShape(CLOSE)
+        rect(self.getX()+4, self.getY()+4, self.r-8, self.r-8)
+        
